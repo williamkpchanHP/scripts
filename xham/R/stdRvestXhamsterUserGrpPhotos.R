403d871674c0cf3ca5070c24e09977934043fb43
williamkpchanHP <williamkpchanHP@gmail.com>
2024-09-30 17:22:03 +0800

    update

--- xham/R/stdRvestXhamsterUserGrpPhotos.R
+++ xham/R/stdRvestXhamsterUserGrpPhotos.R
@@ -16,45 +16,8 @@ library(jsonlite)
 
 userHeader="https://xhamster.com/users/"
 lineSignature = "window.initials="
-
-userName = "olderwomanfun"
-userName = readline(prompt="enter userName: ")
-userOrCreat = readline(prompt="user or creator? 0/1: ")
-if(userOrCreat==0){
-  userHeader="https://xhamster.com/users/"
-}else{
-  userHeader="https://xhamster.com/creators/"
-}
-
-wholePage = character()
-allLinks = character()
-
-theFilename = paste0("xhamFoto ", userName, " List.html")
-url = paste0(userHeader, userName, "/photos/")
-
 batchFIleHistory = readLines("xhamFotoBatHist.txt")
-historyIdx = grep(userName, batchFIleHistory)
-if(length(historyIdx) !=0){
-    stop(red("\nAlready Listed in xhamFotoBatHist.txt History!: ", historyIdx, "\n"))
-}
-
-cat(url, "\n")
-pagesource <- read_html(url)
-lastPageclassName = ".xh-paginator-button, .page-button-link"
-lastPageNum <- html_nodes(pagesource, lastPageclassName)
-lastPageNum = lastPageNum[length(lastPageNum)]
-if(length(lastPageNum)==0){
-  lastPageNum = 1
-}else{
-  lastPageNum = as.numeric(html_text(lastPageNum))
-}
-
-addr=1:lastPageNum
-lentocpage = lastPageNum
-cat("\nalbum total pages: ",lentocpage,"\n")
-
 ProcessStartTime = Sys.time()
-cat(format(Sys.time(), "%H:%M:%OS"),"\n")
 
 dhms <- function(t){
     paste(t %/% (60*60*24), "day" 
@@ -66,92 +29,177 @@ dhms <- function(t){
          )
 }
 
-for(i in addr){
- cat(i, "of", length(addr), " ")
- if(i==1){
-   url = paste0(userHeader, userName, "/photos/")
- }else{
-   url = paste0(userHeader, userName, "/photos/", i)
- }
+fetchUserFotos = function(userName){
+  url = paste0(userHeader, userName, "/photos/")
+  historyIdx = grep(userName, batchFIleHistory)
+  if(length(historyIdx) !=0){
+     stop(red("\nAlready Listed in xhamFotoBatHist.txt History!: ", historyIdx, "\n"))
+  }
 
- cat("url:",url, "\n")
- pagesource <- readLines(url)
- targetLineIdx = grep(lineSignature, pagesource)
- targetLine = pagesource[targetLineIdx]
- targetLine = gsub('^.*userGalleriesCollection":', '', targetLine )
- targetLine = gsub(',"favoritesGalleryCollection.*', '', targetLine)
- targetLine = gsub(',"pagination".*', '', targetLine)
+  cat(red(url, "\n"))
+  pagesource <- read_html(url)
+  lastPageclassName = ".xh-paginator-button, .page-button-link"
+  lastPageNum <- html_nodes(pagesource, lastPageclassName)
+  lastPageNum = lastPageNum[length(lastPageNum)]
+  if(length(lastPageNum)==0){
+    lastPageNum = 1
+  }else{
+    lastPageNum = as.numeric(html_text(lastPageNum))
+  }
+
+  addr=1:lastPageNum
+  lentocpage = lastPageNum
+  cat("\nalbum total pages: ",lentocpage,"\n")
+  cat(format(Sys.time(), "%H:%M:%OS"),"\n")
+
+
+  for(i in addr){
+   cat(i, "of", length(addr), " ")
+   if(i==1){
+     url = paste0(userHeader, userName, "/photos/")
+   }else{
+     url = paste0(userHeader, userName, "/photos/", i)
+   }
+
+   cat("url:",url, "\n")
+   pagesource <- readLines(url)
+   targetLineIdx = grep(lineSignature, pagesource)
+   targetLine = pagesource[targetLineIdx]
+   targetLine = gsub('^.*userGalleriesCollection":', '', targetLine )
+   targetLine = gsub(',"favoritesGalleryCollection.*', '', targetLine)
+   targetLine = gsub(',"pagination".*', '', targetLine)
  
-# writeClipboard(targetLine)
- targetLineDf = fromJSON(targetLine)
+  # writeClipboard(targetLine)
+   targetLineDf = fromJSON(targetLine)
 
- # "icon":"friends", "privacy":1,
- # "icon":"lock",
- if("icon" %in% colnames(targetLineDf)){
+   # "icon":"friends", "privacy":1,
+   # "icon":"lock",
+   if("icon" %in% colnames(targetLineDf)){
     privacy = unlist(targetLineDf["icon"])
     rmIdx = grep("friends|lock", privacy)
     if(length(rmIdx)>0){
       targetLineDf =  targetLineDf[-rmIdx, ]
     }
- }
+   }
 
- links = unlist(targetLineDf["pageURL"])
- imgSrc = unlist(targetLineDf["thumbURL"])
- linksTxt = unlist(targetLineDf["title"])
- linksTxt = gsub("'", "\'", linksTxt)
- linksTxt = gsub(" from OlderWomanFun", "", linksTxt)
+   links = unlist(targetLineDf["pageURL"])
+   imgSrc = unlist(targetLineDf["thumbURL"])
+   linksTxt = unlist(targetLineDf["title"])
+   linksTxt = gsub("'", "\'", linksTxt)
+   linksTxt = gsub(" from OlderWomanFun", "", linksTxt)
 
- if("imgCount" %in% colnames(targetLineDf)){
+   if("imgCount" %in% colnames(targetLineDf)){
     imgCount = unlist(targetLineDf["imgCount"])
     result = paste0('000',imgCount,'<div><a href="', links, '"><img src="', imgSrc, '"><br>', linksTxt, '</a> ', imgCount, '</div>')
- }else{
+   }else{
     result = paste0('<div><a href="', links, '"><img src="', imgSrc, '"><br>', linksTxt, '</a></div>')
- }
+   }
 
- cat("length(resut): ", length(result), "\n")
- wholePage = c(wholePage, result)
- allLinks = c(allLinks, links)
+   cat("length(resut): ", length(result), "\n")
+   wholePage <<- c(wholePage, result)
+   allLinks <<- c(allLinks, links)
 
- if(i == 10){
-   ProcessEndTime = Sys.time()
-   LoopTime = as.numeric(ProcessEndTime - ProcessStartTime, units="secs")
-   ecTime = length(addr)*LoopTime/10
+   if(i == 10){
+     ProcessEndTime = Sys.time()
+     LoopTime = as.numeric(ProcessEndTime - ProcessStartTime, units="secs")
+     ecTime = length(addr)*LoopTime/10
 
-   cat(red(
+     cat(red(
         "\n\n Expect to complete at: ", as.character(ProcessStartTime + ecTime),"\n",
         "per cycle time: ", dhms(LoopTime/10),"\n",
         "total time: ", dhms(ecTime),"\n\n"
       ))
- }
+   }
+  }
+  saveFile(userName)
 }
 
+saveFile = function(userName){
+  theFilename = paste0("xhamFoto ", userName, " List.html")
+  #writeClipboard(wholePage)
+  setwd("C:/Users/william/Desktop/scripts/xham/R")
+  templateHead = readLines("templateHeadXham.txt")
+  templateTail = readLines("templateTailXham.txt")
+  templateHead = gsub("mom50", userName, templateHead)
+  templateTail = gsub("mom50", userName, templateTail)
 
-#writeClipboard(wholePage)
-templateHead = readLines("templateHeadXham.txt")
-templateTail = readLines("templateTailXham.txt")
-templateHead = gsub("mom50", userName, templateHead)
-templateTail = gsub("mom50", userName, templateTail)
+  templateTail = gsub("lineHeader = '<div>'", "lineHeader = ''", templateTail)
+  templateTail = gsub("lineTail = '</div>'", "lineTail = ''", templateTail)
 
-wholePage = gsub("'", ".", wholePage)
-wholePage = gsub("</div>", "',", wholePage)
-wholePage = sort(wholePage, decreasing=TRUE)
-wholePage = gsub("^.*?<div>", "'", wholePage)
+  showRangeIdx = grep("showRange", templateTail)
+  if(length(wholePage)<=60){
+    templateTail[showRangeIdx] = paste0("showRange = ", length(wholePage))
+  }
+  wholePage <<- gsub("'", ".", wholePage)
+  wholePage <<- gsub("</div>", "<br>',", wholePage)
+  wholePage <<- sort(wholePage, decreasing=TRUE)
+  wholePage <<- gsub("^.*?<div>", "'", wholePage)
 
-
-setwd("C:/Users/william/Desktop/scripts/xham")
-  sink(theFilename)
-  cat(templateHead, sep="\n")
-  cat(wholePage, sep="\n")
-  cat(templateTail, sep="\n")
-  sink()
+  setwd("C:/Users/william/Desktop/scripts/xham")
+    sink(theFilename)
+      cat(templateHead, sep="\n")
+      cat(wholePage, sep="\n")
+      cat(templateTail, sep="\n")
+    sink()
 
   sink(paste0(userName, "allLinks.txt"))
-  cat(allLinks, sep="\n")
+    cat(allLinks, sep="\n")
   sink()
 
-ProcessEndTime = Sys.time()
-cat(format(Sys.time(), "%H:%M:%OS"),"\n")
-LoopTime = trunc(as.numeric(ProcessEndTime - ProcessStartTime, units="secs"))
-cat("Task completed! loop time: ", dhms(LoopTime),"\n\n\n")
-cat(red(theFilename, "created!", "Total links: ", length(wholePage), "\n"))
-cat(yellow("Total links: ", length(allLinks), " in file: ", paste0(userName, "allLinks.txt")))
+  ProcessEndTime = Sys.time()
+  cat(format(Sys.time(), "%H:%M:%OS"),"\n")
+  LoopTime = trunc(as.numeric(ProcessEndTime - ProcessStartTime, units="secs"))
+  cat("Task completed! loop time: ", dhms(LoopTime),"\n\n\n")
+  cat(red(theFilename, "created!", "Total links: ", length(wholePage), "\n"))
+  cat(yellow("Total links: ", length(allLinks), " in file: ", paste0(userName, "allLinks.txt")))
+}
+
+# main loop
+# ==========
+wholePage = character()
+allLinks = character()
+userHeader = character()
+
+modetype = readline("batch mode or single user? 0/1 ")
+if(modetype=="1"){
+  userName = "olderwomanfun"
+  userName = readline(prompt="enter userName: ")
+  userOrCreat = readline(prompt="user or creator? 0/1: ")
+  if(userOrCreat==0){
+    userHeader="https://xhamster.com/users/"
+  }else{
+    userHeader="https://xhamster.com/creators/"
+  }
+  ProcessStartTime <<- Sys.time()
+  wholePage = character()
+  fetchUserFotos(userName)
+}else{
+  batchFilename = readline("enter short file name, press enter for default, batchurl.txt: ")
+    if(batchFilename==""){
+      batchurl = "batchurl.txt"
+    }else{
+      batchurl = paste0(batchFilename, ".txt")
+    }
+
+  setwd("C:/Users/william/Desktop/scripts/xham")
+  batchuserName = readLines(batchurl)
+  batchUNlen = length(batchuserName)
+  cat("Total userNamea: ", batchUNlen, "\n")
+
+  userOrCreat = readline(prompt="user or creator? 0/1: ")
+  if(userOrCreat==0){
+    userHeader="https://xhamster.com/users/"
+  }else{
+    userHeader="https://xhamster.com/creators/"
+  }
+
+  ProcessStartTime <<- Sys.time()
+  counter = 0
+  for(i in batchuserName){
+    counter = counter + 1
+    cat(counter, "of", batchUNlen,"\n")
+    wholePage = character()
+    fetchUserFotos(i)
+  }
+}
+
